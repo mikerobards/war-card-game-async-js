7ef3ddb553b3a8652bfd66381b3c73d7669a2d24
Michael Robards <mikerobards@icloud.com>
2021-06-24 15:53:43 -0400

    setTimeout

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const button = document.getElementById('new-deck')
 
-button.addEventListener('click', () => {
+const handleClick = () => {
     fetch('https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/')
         .then(res => res.json())
         .then(data => console.log(data))
-})
+}
+
+button.addEventListener('click', handleClick)
+
+const timer = () => {
+    console.log("I finally ran!")
+}
+
+setTimeout(timer, 2000)
+
