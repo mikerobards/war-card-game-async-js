0945dbe5c89f42e6487f8f97a56a6f7980a2204d
Michael Robards <mikerobards@icloud.com>
2021-06-25 16:53:22 -0400

    add winner el and func

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const button = document.getElementById('new-deck')
 const drawBTN = document.getElementById('draw-cards')
 const card1 = document.querySelector('.card-1')
 const card2 = document.querySelector('.card-2')
+const winnerEl = document.querySelector('.winner')
+const cardValue = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'JACK', 'QUEEN', 'KING', 'ACE']
 let deckID = ''
 
 const handleClick = () => {
@@ -20,14 +22,23 @@ const drawCards = () => {
             console.log(data)
             card1.innerHTML = `<img src=${data.cards[0].image}>`
             card2.innerHTML = `<img src=${data.cards[1].image}>`
+            winnerEl.innerText = scoreCards(data.cards[0].value, data.cards[1].value)
+
         })
     console.log(deckID)
 
 }
 
+const scoreCards = (a, b) => {
+    if (cardValue.indexOf(a) > cardValue.indexOf(b)) {
+        return `Computer Wins!`
+    } else if (cardValue.indexOf(b) > cardValue.indexOf(a)) {
+        return `You Win!`
+    } else {
+        return 'WAR!'
+    }
+}
+
 
 button.addEventListener('click', handleClick)
 drawBTN.addEventListener('click', drawCards)
-
-
-
